dc1dbfb7f79c5559a5a956b0d3b8279eb24854cc
Matheus Santos <matheus_sa_santos@hotmail.com>
2019-10-09 14:03:47 -0300

    bootcamp

---
+++ src/app/controller/NotificationController.js
@@ -0,0 +1,37 @@
+import Notification from '../schemas/Notifacation';
+import User from '../models/User';
+
+class NotificationController {
+  async index(req, res) {
+    // Check if provider_id is a provider
+
+    const checkisProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkisProvider) {
+      return res
+        .status(401)
+        .json({ error: 'only provider can load notifications' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
